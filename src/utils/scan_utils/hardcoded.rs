7a7d98bab6f7d2879fbf4a3560376536e3ee94bc
Fishgame Studio <popxh@outlook.com>
2026-08-17 18:48:09 +0800

    :recycle: Refactor code with struct Rule

--- src/utils/scan_utils/hardcoded.rs
+++ src/utils/scan_utils/hardcoded.rs
@@ -2,34 +2,51 @@
 
 use regex::Regex;
 
-use crate::utils::scan_utils::secrets::diagnostic;
+use super::utils::{Rule, diagnostic_by_regex};
+use crate::utils::scan::ItemType as it;
 
-use lazy_static::lazy_static;
+use once_cell::sync::Lazy;
 
-lazy_static! {
-    /// Path
-    static ref PATH: Vec<Regex> = vec![
-        Regex::new(r#"["'`] [A-Za-z]:[\\/][^"'`]*["'`]"#).unwrap(),
-        Regex::new(r#"["'`]([A-Za-z]:[\\/][^"'`]*|/(?:[^"'`\\]+))["'`]"#).unwrap(),
+/// Path
+pub static PATH: Lazy<Rule> = Lazy::new(|| {
+    Rule::new(
+        "Hard-coded Path",
+        it::Hardcoded,
+        vec![
+            Regex::new(r#"["'`] [A-Za-z]:[\\/][^"'`]*["'`]"#).unwrap(),
+            Regex::new(r#"["'`]([A-Za-z]:[\\/][^"'`]*|/(?:[^"'`\\]+))["'`]"#).unwrap(),
+        ],
+    )
+});
 
-    ];
-    /// URI
-    static ref URI: Vec<Regex> = vec![
-        Regex::new(r#"https?://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(/[^\s"'<>]*)?"#).unwrap(),
-        Regex::new(r#"file:///[A-Za-z]:[/\\][^"'`\s]*"#).unwrap(),
-    ];
-    static ref OTHER: Vec<Regex> = vec![
+/// URI
+pub static URI: Lazy<Rule> = Lazy::new(|| {
+    Rule::new(
+        "Hard-coded URI",
+        it::Hardcoded,
+        vec![
+            Regex::new(r#"https?://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(/[^\s"'<>]*)?"#).unwrap(),
+            Regex::new(r#"file:///[A-Za-z]:[/\\][^"'`\s]*"#).unwrap(),
+        ],
+    )
+});
+pub static OTHER: Lazy<Rule> = Lazy::new(|| {
+    Rule::new(
+        "Hard-coded content",
+        it::Hardcoded,
         // Email address
-        Regex::new(r#"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b"#).unwrap(),
-        // Time zone
-        Regex::new(r#"\b(UTC|GMT)[+-]\d{1,2}(:\d{2})?\b"#).unwrap(),
-    ];
-}
+        vec![
+            Regex::new(r#"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b"#).unwrap(),
+            // Time zone
+            Regex::new(r#"\b(UTC|GMT)[+-]\d{1,2}(:\d{2})?\b"#).unwrap(),
+        ],
+    )
+});
 
 pub fn scan_hardcoded(s: &String, filename: &String) -> i32 {
     let mut total_error: i32 = 0;
-    total_error += diagnostic("Hard-coded path", &*PATH, s, filename);
-    total_error += diagnostic("Hard-coded URI", &*URI, s, filename);
-    total_error += diagnostic("Hard-coded content", &*OTHER, s, filename);
+    total_error += diagnostic_by_regex(&*PATH, s, filename);
+    total_error += diagnostic_by_regex(&*URI, s, filename);
+    total_error += diagnostic_by_regex(&*OTHER, s, filename);
     total_error
 }
