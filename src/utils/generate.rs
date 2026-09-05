819babbd35bc881f10cf8a96a3e4936c17cea2ae
Fishgame Studio <popxh@outlook.com>
2026-09-05 15:21:43 +0800

    :sparkles: Add branch for enumeration `Checksum` in the match statement

--- src/utils/generate.rs
+++ src/utils/generate.rs
@@ -1,15 +1,23 @@
 //! Generates specified items.
 
-use crate::utils::gen_utils::{password::gen_password, rand::gen_randfloat, rand::gen_randint};
+use crate::utils::gen_utils::{
+    password::gen_password, rand::gen_randfloat, rand::gen_randint, sha256::gen_checksums,
+    sha256::gen_sha256,
+};
 use crate::utils::logging::{fatal, verbose};
 
+use std::fs::write;
 use std::ops::Range;
+
+use owo_colors::OwoColorize;
 
 #[derive(Debug)]
 pub enum GenerationItemType {
     Password(usize),
     RandInt(Range<i32>),
     RandFloat(Range<f64>),
+    Sha256(String),
+    Checksum,
 }
 
 /// Convert an item name to a enumeration.
@@ -20,6 +28,7 @@ pub fn str2enum(
     password_len: Option<usize>,
     randint_range: Option<Range<i32>>,
     randfloat_range: Option<Range<f64>>,
+    content: Option<String>,
 ) -> GenerationItemType {
     verbose!("Matching string {s}");
     use GenerationItemType::*;
@@ -36,6 +45,13 @@ pub fn str2enum(
             Some(range) => range,
             None => fatal!("Argument 'range' is required if the item specified as 'randfloat'"),
         }),
+        "sha256" => Sha256(match content {
+            Some(content) => content,
+            None => fatal!(
+                "One of arguments 'content' and 'from-file' is required if the item specified as 'sha256'"
+            ),
+        }),
+        "checksum" => Checksum,
         item => fatal!("Unknown type of item: {item}"),
     }
 }
@@ -48,5 +64,20 @@ pub fn generate(item: GenerationItemType) -> String {
         Password(len) => gen_password(len),
         RandInt(range) => gen_randint(range).to_string(),
         RandFloat(range) => gen_randfloat(range).to_string(),
+        Sha256(content) => gen_sha256(&content),
+        Checksum => match gen_checksums() {
+            Ok(s) => {
+                // Write to the file `SHA256SUMS` first.
+                const CHECKSUMS_FILE: &str = "./SHA256SUMS";
+                verbose!("Saving checksums to file {CHECKSUMS_FILE:?} ...");
+                if let Err(e) = write("./SHA256SUMS", &s) {
+                    fatal!("Failed to save checksums to file {CHECKSUMS_FILE}: {e}");
+                } else {
+                    println!("{} {CHECKSUMS_FILE}", "Saved checksums to file: ".green());
+                }
+                String::new() // Return an empty string and nothing will printed later.
+            }
+            Err(e) => fatal!("Failed to generate checksums: {e}"),
+        },
     }
 }
