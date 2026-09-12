59ce146d3c2740194ee75e28eddd2c113ba1ce22
Fishgame Studio <popxh@outlook.com>
2026-09-12 13:12:20 +0800

    :sparkles: Update format of the version

--- src/utils/version.rs
+++ src/utils/version.rs
@@ -1,9 +1,11 @@
 //! A file to store version centrally.
 
 pub const VERSION: &str = concat!(
-    "{} ({}) [{}] on {}",
     env!("CARGO_PKG_VERSION"),
+    " (",
     env!("BUILD_TIME"),
+    ") [",
     env!("TARGET_ARCH"),
+    "] on ",
     env!("TARGET_OS")
 );
